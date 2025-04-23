2e09418be2ab0089bf482d09217fe029708dfe97
rlw <98868435+therlw@users.noreply.github.com>
2025-04-23 15:12:54 +0300

    Delete VeltoUI/Utilities directory

--- VeltoUI/Utilities/Stroke.lua
+++
@@ -1,9 +0,0 @@
-local Theme = require(script.Parent.Parent.Themes.Default)
-
-return function(parent, thickness, color)
-    local stroke = Instance.new("UIStroke")
-    stroke.Thickness = thickness or 1
-    stroke.Color = color or Theme.Secondary
-    stroke.Parent = parent
-    return stroke
-end
